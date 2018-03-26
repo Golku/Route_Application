a6d1dfe77f18581a6bb212d9aae88fbbc18ff0f7
Golku <Jason.devers@hotmail.com>
2018-03-26 13:28:38 +0200

    Merge branch 'di_with_BaseActivity' of C:\Users\Anibal\AndroidStudioProjects\Route_Application_Kotlin with conflicts.

--- app/src/main/java/com/example/jason/route_application_kotlin/features/route/RouteActivity.java
+++ app/src/main/java/com/example/jason/route_application_kotlin/features/route/RouteActivity.java
@@ -6,13 +6,21 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.jason.route_application_kotlin.R;
 import com.example.jason.route_application_kotlin.data.pojos.OrganizedRoute;
 import com.example.jason.route_application_kotlin.features.addressDetails.AddressDetailsActivity;
 import javax.inject.Inject;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dagger.android.support.DaggerAppCompatActivity;
+import okhttp3.Route;
 
 public class RouteActivity extends DaggerAppCompatActivity implements
         MvpRoute.View,
@@ -32,21 +40,13 @@ public class RouteActivity extends DaggerAppCompatActivity implements
 
     private void init(){
         String routeCode = getIntent().getStringExtra("routeCode");
-        presenter.setRouteCode(routeCode);
-        presenter.getRouteFromApi();
-    }
 
-    @Override
-    public void setupFragments(OrganizedRoute organizedRoute) {
-
-        Bundle organizedRouteBundle = new Bundle();
-        organizedRouteBundle.putParcelable("organizedRoute", organizedRoute);
-
-        Fragment routeListFragment = new RouteListFragment();
         Fragment routeMapFragment = new RouteMapFragment();
+        Fragment routeListFragment = new RouteListFragment();
 
-        routeListFragment.setArguments(organizedRouteBundle);
-        routeMapFragment.setArguments(organizedRouteBundle);
+//        RouteListFragment routeListFragmentCaller = getSupportFragmentManager().findFragmentById(R.id.list);
+
+//        routeMapFragment.setArguments();
 
         RouteSectionPagerAdapter routeSectionPagerAdapter = new RouteSectionPagerAdapter(getSupportFragmentManager());
         routeSectionPagerAdapter.addFragment("Route List", routeListFragment);
@@ -57,6 +57,14 @@ public class RouteActivity extends DaggerAppCompatActivity implements
 
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+//        presenter.setRouteCode(routeCode);
+//        presenter.getRouteFromApi();
+    }
+
+    @Override
+    public void setUpAdapter(OrganizedRoute organizedRoute) {
+
     }
 
     @Override
